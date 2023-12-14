24b87817058eb9bc3fb7aa21a3e2bd313e422cfa
NguyenXuanThuong <nxthuong1901@gmail.com>
2023-12-14 17:42:57 +0700

    fix frondEnd #1

--- fe_tescareesreport/src/components/Reports/DepartmentReport.jsx
+++ fe_tescareesreport/src/components/Reports/DepartmentReport.jsx
@@ -15,7 +15,7 @@ import ChartDepartmentReport from "./ChartDepartReport";
 const DepartmentReport = () => {
   const [departments, setDepartments] = useState([]);
   const [departmentSelect, setDepartmentSelect] = useState(1);
-  const [departmentReport, setDepartmentReport] = useState({});
+  const [departmentReport, setDepartmentReport] = useState(null);
 
   const [dataChartGeneral, setDataChartGeneral] = useState([]);
   const [dataChartProfile, setDataChartProfile] = useState([]);
@@ -29,21 +29,31 @@ const DepartmentReport = () => {
   const handleReport = async () => {
     await api.post(`report/department/${departmentSelect}`).then((res) => {
       setDepartmentReport(res.data);
+
       setDataChartGeneral([
-        { y: res.data.hiredCandidatesRate.toFixed(2) * 100, label: "Đậu" },
-        { y: res.data.rejectedCandidatesRate.toFixed(2) * 100, label: "Bị loại" },
+        { y: res.data.hiredCandidatesRate * 100, label: "Đậu" },
+        {
+          y: res.data.rejectedCandidatesRate * 100,
+          label: "Bị loại",
+        },
       ]);
       setDataChartProfile([
-        { y: res.data.passingProfileRate.toFixed(2) * 100, label: "Hồ sơ đậu yêu cầu" },
-        { y: res.data.failProfileRate.toFixed(2) * 100, label: "Hồ sơ không đạt yêu cầu" },
+        {
+          y: res.data.passingProfileRate * 100,
+          label: "Hồ sơ đậu yêu cầu",
+        },
+        {
+          y: res.data.failProfileRate * 100,
+          label: "Hồ sơ không đạt yêu cầu",
+        },
       ]);
       setDataChartPassProfile([
         {
-          y: res.data.hiredCandidatePassProfRate.toFixed(2) * 100,
+          y: res.data.hiredCandidatePassProfRate * 100,
           label: "Đậu theo yêu cầu",
         },
         {
-          y: res.data.rejectedCandidatePassProfRate.toFixed(2) * 100,
+          y: res.data.rejectedCandidatePassProfRate * 100,
           label: "Bị loại theo yêu cầu",
         },
       ]);
@@ -90,71 +100,90 @@ const DepartmentReport = () => {
           <button onClick={handleReport}>Thống kê</button>
         </div>
       </div>
-      <div className="chart">
-        <ChartDepartmentReport
-          data={dataChartGeneral}
-          title={"Tổng quan tỉ lệ tuyển dụng"}
-        />
-      </div>
-      <div className="chart">
-        <ChartDepartmentReport
-          data={dataChartProfile}
-          title={"Tỷ lệ hồ sơ theo yêu cầu phòng ban"}
-        />
-      </div>
-      <div className="chart">
-        <ChartDepartmentReport
-          data={dataChartPassProfile}
-          title={"Tỷ lệ tuyển dụng theo hồ sơ đạt yêu cầu"}
-        />
-      </div>
-      <div className="table-department-report">
-        <div className="title-table">
-          <p>Bảng thống kê</p>
+      {dataChartGeneral.length > 0 && (
+        <div className="chart">
+          <ChartDepartmentReport
+            data={dataChartGeneral}
+            title={"Tổng quan tỉ lệ tuyển dụng"}
+          />
         </div>
+      )}
+      {dataChartProfile.length > 0 && (
+        <div className="chart">
+          <ChartDepartmentReport
+            data={dataChartProfile}
+            title={"Tỷ lệ hồ sơ theo yêu cầu phòng ban"}
+          />
+        </div>
+      )}
+      {dataChartPassProfile.length > 0 && (
+        <div className="chart">
+          <ChartDepartmentReport
+            data={dataChartPassProfile}
+            title={"Tỷ lệ tuyển dụng theo hồ sơ đạt yêu cầu"}
+          />
+        </div>
+      )}
 
+      {departmentReport !== null && (
         <div>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center">Tổng số ứng viên ứng tuyển</TableCell>
-                  <TableCell align="center">Số ứng viên đạt yêu cầu</TableCell>
-                  <TableCell align="center">Số ứng viên không đạt yêu cầu</TableCell>
-                  <TableCell align="center">Số ứng viên được tuyển</TableCell>
-                  <TableCell align="center">Số ứng viên đạt yêu cầu bị loại</TableCell>
-                  <TableCell align="center">Tổng số ứng viên bị loại</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell align="center">
-                    {departmentReport.numberOfApplicants}
-                  </TableCell>
-                  <TableCell align="center">
-                    {departmentReport.numberOfPassingProfile}
-                  </TableCell>
-                  <TableCell align="center">
-                    {departmentReport.numberOfFailProfile}
-                  </TableCell>
-                  
-                  <TableCell align="center">
-                    {departmentReport.numberOfHiredCandidates}
-                  </TableCell>
-                  <TableCell align="center">
-                    {departmentReport.numberOfRejectedCandidates - departmentReport.numberOfFailProfile}
-                  </TableCell>
-                  <TableCell align="center">
-                    {departmentReport.numberOfRejectedCandidates}
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
+          <div className="table-department-report">
+            <div className="title-table">
+              <p>Bảng thống kê</p>
+            </div>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">
+                      Tổng số ứng viên ứng tuyển
+                    </TableCell>
+                    <TableCell align="center">
+                      Số ứng viên đạt yêu cầu
+                    </TableCell>
+                    <TableCell align="center">
+                      Số ứng viên không đạt yêu cầu
+                    </TableCell>
+                    <TableCell align="center">Số ứng viên được tuyển</TableCell>
+                    <TableCell align="center">
+                      Số ứng viên đạt yêu cầu bị loại
+                    </TableCell>
+                    <TableCell align="center">
+                      Tổng số ứng viên bị loại
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell align="center">
+                      {departmentReport.numberOfApplicants}
+                    </TableCell>
+                    <TableCell align="center">
+                      {departmentReport.numberOfPassingProfile}
+                    </TableCell>
+                    <TableCell align="center">
+                      {departmentReport.numberOfFailProfile}
+                    </TableCell>
+
+                    <TableCell align="center">
+                      {departmentReport.numberOfHiredCandidates}
+                    </TableCell>
+                    <TableCell align="center">
+                      {departmentReport.numberOfRejectedCandidates -
+                        departmentReport.numberOfFailProfile}
+                    </TableCell>
+                    <TableCell align="center">
+                      {departmentReport.numberOfRejectedCandidates}
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
